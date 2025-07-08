613be75b18d9cf8ca1f4277edccad394e108a197
Preetraj Haldar <preetrajhaldar2002@gmail.com>
2025-07-09 03:56:54 +0530

    browser and project fix

--- src/app/components/projects/projectsData.ts
+++ src/app/components/projects/projectsData.ts
@@ -2,32 +2,41 @@ export const projects = [
   {
     title: "App Explorer",
     domain: "Full Stack",
+    description: "A penetration testing tool built with Python.",
     iframeUrl: "https://yourdomain.com/project1",
     tags: ["React", "Next.js", "Design System"],
+    techStack: ["Python", "Nmap", "Wireshark"],
+
   },
   {
     title: "Cyber Sentinel",
     domain: "Cyber Security",
+    description: "A penetration testing tool built with Python.",
     iframeUrl: "https://yourdomain.com/project2",
     tags: ["Cyber", "Realtime", "Monitoring"],
+        techStack: ["Python", "Nmap", "Wireshark"],
   },
   {
     title: "Design Vault",
     domain: "Graphic Design",
+    description: "A penetration testing tool built with Python.",
     iframeUrl: "https://yourdomain.com/project3",
     tags: ["Figma", "UI Kit", "Branding"],
+        techStack: ["Python", "Nmap", "Wireshark"],
   },
   {
     domain: 'Full Stack',
     title: 'KhelConnect',
     description: 'Turf booking and payment app',
     iframeUrl: 'https://khelconnect.in/',
+        techStack: ["Python", "Nmap", "Wireshark"],
   },
   {
     domain: 'Cyber Security',
     title: 'Phish & Chips',
     description: 'Red team phishing simulator',
     iframeUrl: 'https://the-threshold-frequency.github.io/',
+        techStack: ["Python", "Nmap", "Wireshark"],
   },
   // Add more here over time...
 ];
